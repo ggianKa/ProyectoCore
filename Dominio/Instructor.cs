a723c26bb2feffcba4baa4005e0497e990c448e8
ggianKa <giancarlo_olhf@outlook.es>
2024-08-20 12:01:08 -0500

    Cambios 30

--- Dominio/Instructor.cs
+++ Dominio/Instructor.cs
@@ -7,6 +7,10 @@ namespace Dominio
 {
     public class Instructor
     {
-        
+        public int InstructorId {get;set;}
+        public string Nombre {get;set;}
+        public string Apellidos {get;set;}
+        public string Grado {get;set;}
+        public byte[] FotoPerfil {get;set;}
     }
 }
