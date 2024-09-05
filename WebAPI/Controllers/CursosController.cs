235586e7918c15d8ac1745f7ef8bf9f75163a905
ggianKa <giancarlo_olhf@outlook.es>
2024-09-04 21:39:20 -0500

    Se creo clase Eliminar Cursos - PCTIC 43

--- WebAPI/Controllers/CursosController.cs
+++ WebAPI/Controllers/CursosController.cs
@@ -8,37 +8,62 @@ using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
 {
-    // http://localhost:5000/api/Cursos
+    // Controlador de API que maneja todas las operaciones relacionadas con los cursos.
+    // La ruta base de este controlador es: http://localhost:5000/api/Cursos
+    // Se utiliza la anotación [ApiController] para mejorar la validación y el manejo de solicitudes HTTP.
     [Route("api/[controller]")]
     [ApiController]
    
     public class CursosController : ControllerBase
     {
         private readonly IMediator _mediator;
+
+        // Constructor que recibe una instancia de IMediator, utilizada para manejar las operaciones
+        // sin que el controlador tenga que implementar la lógica de negocio directamente.
         public CursosController(IMediator mediator)
         {
             _mediator = mediator;
         }
 
+        // Obtiene una lista de objetos cursos
         [HttpGet]
         public async Task<ActionResult<List<Curso>>> Get()
         {
             return await _mediator.Send(new Consulta.ListaCursos());
         }
 
-
+        // Obtiene el detalle de un curso específico, dado su Id
         // http://localhost:5000/api/Cursos/{id}
         // http://localhost:5000/api/Cursos/1
         [HttpGet("{id}")]
         public async Task<ActionResult<Curso>> Detalle(int id)
         {
+            // Envía una solicitud para obtener el curso con el ID proporcionado
             return await _mediator.Send(new ConsultaId.CursoUnico { Id = id });
         }
 
+        // Crea un nuevo curso en la base de datos, los datos estan encapsulados en el objeto Nuevo.Ejecuta
+        // retorna una respuesta de tipo Unit indicando el exito o fallo de operación
         [HttpPost]
         public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data)
         {
+            // Envia una solicitud para crear un nuevo curso utilizando los datos proporcionados
             return await _mediator.Send(data);
+        }
+
+        // Endpoint PUT para editar un curso existente.
+        // El ID del curso se pasa como parámetro en la URL.
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Editar(int id, Editar.Ejecuta data)
+        {
+            data.CursoId = id;
+            return await _mediator.Send(data);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Eliminar(int id)
+        {
+            return await _mediator.Send(new Eliminar.Ejecuta { Id = id });
         }
     }
 }
